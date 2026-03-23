1688f80721d13317dc48ccbbb38b22ba00b36109
aungkokodev <aungkokoepic@gmail.com>
2026-03-24 04:52:22 +0630

    Add /test route

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get('/greet', (req, res) => {
   res.send('<h1>Hello!</h1>')
 })
 
+app.get('/ding', (req, res) => {
+  res.send('<h1>Dong</h1>')
+})
+
+app.get('/test', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
 
 const PORT = 8080
 
